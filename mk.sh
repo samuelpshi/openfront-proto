801a6ac51ba9c1f5be4d9452681f5617976811d4
Sam Shi <samuelshi007@gmail.com>
2026-09-21 23:52:53 -0400

    mk.sh: -ffp-contract=off on all builds

--- mk.sh
+++ mk.sh
@@ -5,7 +5,7 @@ INC="-I $P/raylib-5.5_macos/include -I $P/src -I $P/vendor -I $P/ocean/openfront
 RL=$P/raylib-5.5_macos/lib/libraylib.a
 FW="-framework Cocoa -framework IOKit -framework CoreVideo -framework OpenGL"
 SAN="-fsanitize=address,undefined"
-gcc -g -O0 -Wall -Wextra -DDEBUG $SAN $INC harness.c -o of_dbg  $RL $FW -lm
-gcc -g -O0 -Wall -Wextra         $SAN $INC drive_test.c -o drive  $RL $FW -lm
-gcc -O2 -Wall -Wextra                 $INC harness.c -o of_fast $RL $FW -lm
+gcc -g -O0 -Wall -Wextra -ffp-contract=off -DDEBUG $SAN $INC harness.c -o of_dbg  $RL $FW -lm
+gcc -g -O0 -Wall -Wextra -ffp-contract=off         $SAN $INC drive_test.c -o drive  $RL $FW -lm
+gcc -O2 -Wall -Wextra -ffp-contract=off                 $INC harness.c -o of_fast $RL $FW -lm
 echo "built of_dbg + drive + of_fast"
